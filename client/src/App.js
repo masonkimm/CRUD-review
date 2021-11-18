fbd2a08218106e0c7d3a7c4fd9202837401990bd
Mason Kim <sungjinkimm@gmail.com>
2021-11-18 11:49:02 -0500

    basic layout complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [position, setPosition] = useState('');
+  const [techStack, setTechStack] = useState('');
+  const [salary, setSalary] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, age, position, techStack, salary);
+  };
   return (
     <div className='App'>
-      <img src={logo} className='App-logo' alt='logo' />
+      <Header />
+      <div className='container'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <form action='' onSubmit={handleSubmit}>
+          <label htmlFor='name'>Name:</label>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <label htmlFor=''>Age:</label>
+          <input
+            type='number'
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          />
+          <label htmlFor=''>Position:</label>
+          <input
+            type='text'
+            onChange={(e) => {
+              setPosition(e.target.value);
+            }}
+          />
+          <label htmlFor=''>Tech Stack:</label>
+          <input
+            type='text'
+            onChange={(e) => {
+              setTechStack(e.target.value);
+            }}
+          />
+          <label htmlFor=''>Salary:</label>
+          <input
+            type='number'
+            onChange={(e) => {
+              setSalary(e.target.value);
+            }}
+          />
+          <button type='submit'>Add Employee</button>
+        </form>
+      </div>
     </div>
   );
 }
